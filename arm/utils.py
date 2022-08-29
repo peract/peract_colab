1edfcd51ecf27e5e12a77c7cdddda120242b08b8
Mohit Shridhar <mohit.appi@gmail.com>
2022-08-29 16:08:01 -0700

    action

---
+++ arm/utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+
+
+def normalize_quaternion(quat):
+    return np.array(quat) / np.linalg.norm(quat, axis=-1, keepdims=True)
+
+
+def quaternion_to_discrete_euler(quaternion, resolution):
+    euler = Rotation.from_quat(quaternion).as_euler('xyz', degrees=True) + 180
+    assert np.min(euler) >= 0 and np.max(euler) <= 360
+    disc = np.around((euler / resolution)).astype(int)
+    disc[disc == int(360 / resolution)] = 0
+    return disc
+
+
+def point_to_voxel_index(
+        point: np.ndarray,
+        voxel_size: np.ndarray,
+        coord_bounds: np.ndarray):
+    bb_mins = np.array(coord_bounds[0:3])
+    bb_maxs = np.array(coord_bounds[3:])
+    dims_m_one = np.array([voxel_size] * 3) - 1
+    bb_ranges = bb_maxs - bb_mins
+    res = bb_ranges / (np.array([voxel_size] * 3) + 1e-12)
+    voxel_indicy = np.minimum(
+        np.floor((point - bb_mins) / (res + 1e-12)).astype(
+            np.int32), dims_m_one)
+    return voxel_indicy
