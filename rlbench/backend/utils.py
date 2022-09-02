bfbced907011a23e0b37380d061fe5f159b578a7
Mohit Shridhar <mohit.appi@gmail.com>
2022-09-01 17:10:48 -0700

    minor

--- rlbench/backend/utils.py
+++ rlbench/backend/utils.py
@@ -1,4 +1,13 @@
 import numpy as np
+from rlbench.backend.observation import Observation
+
+# Adapted from https://github.com/stepjam/YARR/blob/main/yarr/envs/rlbench_env.py
+
+REMOVE_KEYS = ['joint_velocities', 'joint_positions', 'joint_forces',
+               'gripper_open', 'gripper_pose',
+               'gripper_joint_positions', 'gripper_touch_forces',
+               'task_low_dim_state', 'misc']
+
 
 def image_to_float_array(image, scale_factor=None):
   """Recovers the depth values from an image.
@@ -40,3 +49,61 @@ def image_to_float_array(image, scale_factor=None):
     float_array = image_array.astype(np.float32)
   scaled_array = float_array / scale_factor
   return scaled_array
+
+
+def _extract_obs(obs: Observation,
+                 t: int = 0,
+                 prev_action = None,
+                 channels_last: bool = False):
+
+    obs.joint_velocities = None
+    grip_mat = obs.gripper_matrix
+    grip_pose = obs.gripper_pose
+    joint_pos = obs.joint_positions
+    obs.gripper_pose = None
+    obs.gripper_matrix = None
+    obs.wrist_camera_matrix = None
+    obs.joint_positions = None
+    if obs.gripper_joint_positions is not None:
+        obs.gripper_joint_positions = np.clip(
+            obs.gripper_joint_positions, 0., 0.04)
+
+    obs_dict = vars(obs)
+    obs_dict = {k: v for k, v in obs_dict.items() if v is not None}
+    robot_state = np.array([
+                  obs.gripper_open,
+                  *obs.gripper_joint_positions])
+    # remove low-level proprioception variables that are not needed
+    obs_dict = {k: v for k, v in obs_dict.items()
+                if k not in REMOVE_KEYS}
+    
+    if not channels_last:
+        # swap channels from last dim to 1st dim
+        obs_dict = {k: np.transpose(
+            v, [2, 0, 1]) if v.ndim == 3 else np.expand_dims(v, 0)
+                    for k, v in obs_dict.items()}
+    else:
+        # add extra dim to depth data
+        obs_dict = {k: v if v.ndim == 3 else np.expand_dims(v, -1)
+                    for k, v in obs_dict.items()}
+    obs_dict['low_dim_state'] = np.array(robot_state, dtype=np.float32)
+
+    # binary variable indicating if collisions are allowed or not while planning paths to reach poses
+    obs_dict['ignore_collisions'] = np.array([obs.ignore_collisions], dtype=np.float32)
+    for (k, v) in [(k, v) for k, v in obs_dict.items() if 'point_cloud' in k]:
+        obs_dict[k] = v.astype(np.float32)
+
+    for camera_name in CAMERAS:
+          obs_dict['%s_camera_extrinsics' % camera_name] = obs.misc['%s_camera_extrinsics' % camera_name]
+          obs_dict['%s_camera_intrinsics' % camera_name] = obs.misc['%s_camera_intrinsics' % camera_name]
+
+    # add timestep to low_dim_state
+    time = (1. - (t / float(EPISODE_LENGTH - 1))) * 2. - 1.
+    obs_dict['low_dim_state'] = np.concatenate(
+        [obs_dict['low_dim_state'], [time]]).astype(np.float32)
+
+    obs.gripper_matrix = grip_mat
+    obs.joint_positions = joint_pos
+    obs.gripper_pose = grip_pose
+
+    return obs_dict
